b780c965f12fb0b901d43e73b25230a86794c9e3
Genrevvv <genrevvaguilar@gmail.com>
2025-03-14 22:34:26 +0800

    Update comment checker

--- comment_checker.py
+++ comment_checker.py
@@ -13,11 +13,11 @@ def check_kyedae_comment(string_comment):
     for spelling in spellings:
 
         for word in string_comment:
-            #print(f"{word} : {spelling}")
+            print(f"{word} : {spelling}")
             
             if word == spelling:
                 return True
-            elif fuzz.ratio(word, spelling) >= 70 and word.startswith("k"):
+            elif fuzz.ratio(word, spelling) >= 70 and word.startswith("k") and len(word) >= 5:
                 print(f"\033[1mfuzz ratio: {fuzz.ratio(word, spelling)}\033[0m")
                 return True
 
@@ -25,7 +25,7 @@ def check_kyedae_comment(string_comment):
 
 
 def main():
-    if check_kyedae_comment("iss_tthat..kaedep  from philipphines"):
+    if check_kyedae_comment(" memes review tas nakalive, kada tatawa si luna may parusa😹 "):
         print("Kyedae comment FOUND")
     else:
         print("Kyedae comment NOT FOUND")
