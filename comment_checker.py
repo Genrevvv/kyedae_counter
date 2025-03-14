75afccb8ff520053882a28b219412496648af4a0
Genrevvv <genrevvaguilar@gmail.com>
2025-03-14 21:21:22 +0800

    Simplified comment_checker

--- comment_checker.py
+++ comment_checker.py
@@ -5,32 +5,29 @@ import re
 
 def check_kyedae_comment(string_comment):
     string_comment = string_comment.lower()
-    string_comment = re.sub(r'[^a-zA-Z0-9\s]', '', string_comment)
+    string_comment = re.sub(r'[^a-zA-Z0-9\s]', ' ', string_comment)
+    string_comment = string_comment.split()
 
     spellings = ["kyedae", "kyadae", "kaede"]
 
     substring = ""
     
     for spelling in spellings:
-        if string_comment.find(spelling) != -1:
-            return True
 
-        last_index = len(string_comment) - len(spelling)
-
-        for i in range(last_index + 1):
-            substring = string_comment[i:len(spelling) + i]                
-            # print(f"{substring} : {spelling}")
-
-            if fuzz.ratio(substring, spelling) >= 70 and substring.startswith("k"):
+        for word in string_comment:
+            #print(f"{word} : {spelling}")
+            
+            if word == spelling:
+                return True
+            elif fuzz.ratio(substring, spelling) >= 70 and substring.startswith("k"):
                 print(f"\033[1mfuzz ratio: {fuzz.ratio(substring, spelling)}\033[0m")
                 return True
 
-    # print("kyedae comment not detected")
     return False
 
 
 def main():
-    if check_kyedae_comment("di ka kamo nadedetect comment mo sa program hahaa"):
+    if check_kyedae_comment("iss_tthat..kaedep  from philipphines"):
         print("Kyedae comment FOUND")
     else:
         print("Kyedae comment NOT FOUND")
